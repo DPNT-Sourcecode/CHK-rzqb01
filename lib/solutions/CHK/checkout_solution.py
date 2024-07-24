55818370cb248cf247df19ba3d0d769a49f97b28
sbx_user1051 <sbx_user1051@169.254.94.253>
2024-07-24 09:15:53 +0000

    Wed Jul 24 09:15:53 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,42 +1,26 @@
-from collections import Counter
-from typing import List, Optional
+from collections import Counter, defaultdict
+from dataclasses import dataclass
 
 
-class Offer:
-    pass
-
-
-class MultiOffer(Offer):
-    def __init__(self, sku: str, quantity: int, total_price: int):
-        self.sku = sku
-        self.quantity = quantity
-        self.total_price = total_price
-
-
-class OtherItemOffer(Offer):
-    def __init__(self, sku: str, quantity: int, other_sku: str, other_quantity: int):
-        self.sku = sku
-        self.quantity = quantity
-        self.other_sku = other_sku
-        self.other_quantity = other_quantity
-
-
+@dataclass
 class Item:
-    def __init__(self, sku: str, unit_price: int):
-        self.sku = sku
-        self.unit_price = unit_price
-
-    def calculate_price(self, quantity: int) -> int:
-
-        times_offer_applied, remaining_quantity = divmod(quantity, self.offer.quantity)
-        price_with_offer = times_offer_applied * self.offer.total_price
-        return price_with_offer + remaining_quantity * self.unit_price
+    sku: str
+    unit_price: int
 
 
-class Basket:
-    def __init__(self, skus: List[str]):
-        self.skus = Counter(skus)
+@dataclass
+def MultiOffer:
+    sku: str
+    quantity: int
+    total_price: int
 
+
+@dataclass
+def FreeOffer:
+    sku: str
+    quantity: int
+    target_sku: str
+    target_quantity: int
 
 PRICE_TABLE = {
     "A": Item(sku="A", unit_price=50),
@@ -45,50 +29,53 @@ PRICE_TABLE = {
     "D": Item(sku="D", unit_price=15),
     "E": Item(sku="E", unit_price=40),
 }
-MULTI_OFFERS = [
-    MultiOffer(sku="A", quantity=3, total_price=130),
-    MultiOffer(sku="A", quantity=5, total_price=200),
-    MultiOffer(sku="B", quantity=2, total_price=45),
-]
-OTHER_ITEM_OFFERS = [
-    OtherItemOffer(sku="E", quantity=2, other_sku="B", other_quantity=1),
-]
+MULTI_OFFERS = {
+    "A": MultiOffer(sku="A", quantity=3, total_price=130),
+    "A": MultiOffer(sku="A", quantity=5, total_price=200),
+    "B": MultiOffer(sku="B", quantity=2, total_price=45),
+}
+FREE_OFFERS = {
+    "E": OtherItemOffer(sku="E", quantity=2, target_sku="B", target_quantity=1),
+}
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    # assuming skus looks like ABCD
     basket = Counter(skus)
-    for offer in OTHER_ITEM_OFFERS:
-        quantity_in_basket = basket.get(offer.sku)
-        other_quantity_in_basket = basket.get(offer.other_sku)
-        if (
-                quantity_in_basket is None or
-                quantity_in_basket < offer.quantity or
-                other_quantity_in_basket is None or
-                other_quantity_in_basket < offer.other_quantity
-        ):
-            continue
-        basket[offer.other_sku] -= offer.other_quantity
 
-    prices = {}
-    for offer in MULTI_OFFERS:
-        quantity_in_basket = basket.get(offer.sku)
-        item = PRICE_TABLE.get(offer.sku)
-        if quantity_in_basket is None or item is None or quantity_in_basket < offer.quantity:
+    for sku, offer in FREE_OFFERS.items():
+        quantity = basket.get(sku)
+        if quantity is None or quantity < offer.quantity:
             continue
 
-        times_offer_applied, remaining_quantity = divmod(quantity_in_basket, offer.quantity)
-        price_with_offer = times_offer_applied * offer.total_price
-        prices[offer.sku] = price_with_offer + remaining_quantity * item.unit_price
+        target_quantity = basket.get(offer.target_sku)
+        if target_quantity is None or target_quantity < offer.target_quantity:
+            continue
 
-    total_price = 0
-    for sku, quantity in basket.items():
+        basket[offer.target_sku] -= offer.target_quantity
+
+    prices = defaultdict(int)
+
+    for sku, basket_quantity in basket.items():
+        current_quantity = basket_quantity
         item = PRICE_TABLE.get(sku)
         if item is None:
             return -1
 
-        total_price += item.calculate_price(quantity)
+        multi_offers = [v for v in MULTI_OFFERS.values() if v.sku == sku]
+        if not multi_offers:
+            prices[sku] = current_quantity * item.unit_price
+            continue
 
-    return total_price
+        multi_offers = sorted(multi_offers, key=lambda o: o.quantity, reverse=True)
+        for offer in multi_offers:
+            times_offer_applied, remaining_quantity = divmod(current_quantity, offer.quantity)
+            price_with_offer = times_offer_applied * offer.total_price
+            prices[sku] += price_with_offer
+
+            current_quantity = remaining_quantity
+
+        prices[sku] += current_quantity * item.unit_price
+
+    return sum(prices.values())
